9fa11737bc9bdb8bb5279cf5359ad88c9c024d32
madhaviburla <127477203+madhaviburla@users.noreply.github.com>
2026-06-08 16:52:36 +0530

    Update features.py

--- module_5_compute_engine/feature_repo/features.py
+++ module_5_compute_engine/feature_repo/features.py
@@ -19,14 +19,14 @@ def transform_feature(inputs: pyspark.sql.DataFrame):
             "order_completed",
             when(col("O_ORDERSTATUS") =="F", 1).otherwise(0)
         )
-    print("High value customer feature transformation applied successfully")
-    transformed_df.show(5, truncate=False)
+        print("High value customer feature transformation applied successfully")
+        transformed_df.show(5, truncate=False)
     
-    transformed_df.toPandas().to_csv(
+        transformed_df.toPandas().to_csv(
         "customer_features.csv",
         index=False)
     
-    return transformed_df
+        return transformed_df
 
 
 bfv = BatchFeatureView(
