30de577bf86bf2396740e3fb35c4f266a56bc1e2
madhaviburla <127477203+madhaviburla@users.noreply.github.com>
2026-06-08 18:49:19 +0530

    Update features.py

--- module_5_compute_engine/feature_repo/features.py
+++ module_5_compute_engine/feature_repo/features.py
@@ -1,15 +1,11 @@
 from feast import BatchFeatureView, Field
 from feast.types import String, Float32
-from data_source import *
-from entities import *
 
 bfv = BatchFeatureView(
     name="order_stats",
-    entities=[customer],
     schema=[
         Field(name="O_TOTALPRICE", dtype=Float32),
         Field(name="O_ORDERSTATUS", dtype=String),
     ],
     online=True,
-    source=drivers_stat_source,
 )
