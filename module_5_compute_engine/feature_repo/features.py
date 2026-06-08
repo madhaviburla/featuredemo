a7f7058372de61951396908cf9a3bb0f134d7662
madhaviburla <127477203+madhaviburla@users.noreply.github.com>
2026-06-08 18:31:00 +0530

    Update features.py

--- module_5_compute_engine/feature_repo/features.py
+++ module_5_compute_engine/feature_repo/features.py
@@ -1,50 +1,15 @@
-from datetime import timedelta
-
-from feast import (
-    BatchFeatureView,
-    Field,
-)
-import pyspark
-from feast.types import String, Float32, Int64
-
-from data_sources import *
+from feast import BatchFeatureView, Field
+from feast.types import String, Float32
+from data_source import *
 from entities import *
 
-
-def transform_feature(inputs: pyspark.sql.DataFrame):
-    transformed_df = inputs.withColumn(
-        "high_value_order",
-        when(col("O_TOTALPRICE") >5000, 1).otherwise(0)
-    ).withColumn(
-        "order_completed",
-        when(col("O_ORDERSTATUS") =="F", 1).otherwise(0)
-    )
-    print("High value customer feature transformation applied successfully")
-
-    transformed_df.show(5, truncate=False)
-    
-    transformed_df.toPandas().to_csv(
-        "customer_features.csv",
-        index=False
-    )
-    
-    return transformed_df
-
-
-
 bfv = BatchFeatureView(
-    name="order_stats",
-    description="Hourly features",
+    name="order_stats"
     entities=[customer],
     schema=[
         Field(name="O_TOTALPRICE", dtype=Float32),
         Field(name="O_ORDERSTATUS", dtype=String),
-        Field(name="O_high_value_order", dtype=Int64),
-        Field(name="O_order_completed", dtype=Int64),
     ],
-    udf=transform_feature,
     online=True,
     source=drivers_stat_source,
-    tags={"production": "True"},
-    owner="test2@gmail.com",
 )
