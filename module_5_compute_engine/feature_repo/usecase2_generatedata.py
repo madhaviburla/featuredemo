ee35e490af99ac2f1aa10e8ae8a7d8573e9ca2be
madhaviburla <127477203+madhaviburla@users.noreply.github.com>
2026-06-09 16:41:02 +0530

    Update usecase2_generatedata.py

--- module_5_compute_engine/feature_repo/usecase2_generatedata.py
+++ module_5_compute_engine/feature_repo/usecase2_generatedata.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
 df = pd.DataFrame({
-     "customer_id": [101,102,103],
-     "total_orders": [15,3,8]
+     "customer_id": [101, 102, 103],
+     "total_orders": [15, 3, 8],
 })
 
 def segment_customer(x):
     if x >= 10:
-         return "Premium"
-elif x >= 5:
-       return "Regular"
-else:
-    return "Low"
+        return "Premium"
+   elif x >= 5:
+        return "Regular"
+   else:
+        return "Low"
 
 df["customer_segment"] = df["total_orders"].apply(segment_customer)
 
