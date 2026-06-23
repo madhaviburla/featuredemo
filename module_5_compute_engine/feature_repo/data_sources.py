43ee806f6016c0cdd1825c4df633d58fd52c8b84
madhaviburla <127477203+madhaviburla@users.noreply.github.com>
2026-06-23 14:14:22 +0530

    Update data_sources.py

--- module_5_compute_engine/feature_repo/data_sources.py
+++ module_5_compute_engine/feature_repo/data_sources.py
@@ -17,6 +17,6 @@ from feast.types import Int64, Float32
 
 
 drivers_stat_source = FileSource(
-    path="../data/driver_stats_lat_lon.parquet",
+    path="../data/customer_features.csv",
     timestamp_field="event_timestamp",
 )
