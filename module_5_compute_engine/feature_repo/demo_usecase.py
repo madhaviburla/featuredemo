5c7d1766b052075f496dd83bd7433dd3feb7b555
madhaviburla <127477203+madhaviburla@users.noreply.github.com>
2026-06-24 10:42:45 +0530

    Update demo_usecase.py

--- module_5_compute_engine/feature_repo/demo_usecase.py
+++ module_5_compute_engine/feature_repo/demo_usecase.py
@@ -16,10 +16,25 @@ df["order_completed"] = df["O_ORDERSTATUS"].apply(
     lambda x:1 if x == "F" else 0
 )
 
-print("high valude customer feature")
+# segmentation features
+df["total_orders"] = [15, 5, 20]
+df["customer_segmen"] = ["High", "Low", "High"]
+
+# Churn Features
+df["days_since_last_purchase"] = [5, 20, 2]
+df["last_90d_order_count"] = ["3, 1, 6"]
+df["avg_order_value"] = df["O_TOTALPRICE"]
+df["support_ticket_count"] = [2, 1, 0]
+df["login_count_last_30d"] = [10, 5, 18]
+df["coupon_usage_rate"] = [0.30, 0.10, 0.45]
+
+# Feast source timestamp
+df["event_timestamp"] = "2024-06-01 10:00:00"
+
+print("customer_feature.csv generated successfully")
 
 print(df.to_string(index=False))
 
 df.to_csv("customer_feature.csv", index=False)
 
-print("customer_feature.csv generated successfully")
+
